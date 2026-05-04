fa87c6ec159f4e08fdc9aea835abd02a6f18cb60
Manlikenonso1k <victorynonso9@gmail.com>
2026-05-04 21:43:08 +0100

    Fix guest redirect for portal login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__.'/../app/Console/Commands',
     ])
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return '/portal/login';
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
